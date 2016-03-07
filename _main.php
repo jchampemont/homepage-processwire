a193027566427b85d17bfe1dbc15c8f9be6265c2
Jean Champémont <jchampemont@users.noreply.github.com>
2016-03-07 15:59:11 +0100

    adds piwik tracking code

--- _main.php
+++ _main.php
@@ -30,5 +30,22 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?= $config->urls->templates?>js/bootstrap.min.js"></script>
+    <!-- Piwik -->
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      _paq.push(["setDomains", ["*.www.jeanchampemont.com","*.jeanchampemont.com","*.jeanchampemont.com","*.www.jeanchampemont.com"]]);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//analytics.jeanchampemont.com/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', 3]);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="//analytics.jeanchampemont.com/piwik.php?idsite=3" style="border:0;" alt="" /></p></noscript>
+    <!-- End Piwik Code -->
+
   </body>
 </html>
